87d9784fdb4448b7c3c15a3764feb85981aef452
Claudia Lucia <claudinhaseg@gmail.com>
2023-10-26 16:00:24 -0300

    Desafio Pronto

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-let nome = "Felipe";
-console.log ("oi");
+let nomeHeroi = "Felipão"
+let quantidadeXP = 5555
+let nivelDoHeroi
+
+if(quantidadeXP <= 1000){
+nivelDoHeroi = "Ferro"
+} else if (quantidadeXP >= 1001 && quantidadeXP <= 2000){
+nivelDoHeroi = "Broze"
+} else if (quantidadeXP >= 2001 && quantidadeXP <= 5000){
+    nivelDoHeroi = "Prata"
+} else if (quantidadeXP >= 5001 && quantidadeXP <= 7000){
+    nivelDoHeroi = "Ouro"
+} else if (quantidadeXP >= 7001 && quantidadeXP <= 8000){
+    nivelDoHeroi = "Platina"
+} else if (quantidadeXP >= 8001 && quantidadeXP <= 9000){
+    nivelDoHeroi = "Ascendente"
+} else if (quantidadeXP >= 10001){
+    nivelDoHeroi = "Radiante"
+}
+
+
+
+console.log("O Herói de nome "  + nomeHeroi + " está no nivel de " + nivelDoHeroi )
